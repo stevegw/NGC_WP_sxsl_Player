1c83b2a9ce2db71d9051dc21351b5bb00c58dc61
stevegw <stevegwusa@gmail.com>
2024-07-03 14:31:36 -0600

    minor changes

--- src/phone/components/StartPoint.js
+++ src/phone/components/StartPoint.js
@@ -3,7 +3,7 @@
 
 console.log($scope.app);
 
-/*let pre = [{stepId:"e6aaf568-0fbf-4efe-84f2-82b43fa3a233", status:"Passed"} , {stepId:"c4b3e0b6-21d7-4e5b-aa0c-86103bc3b2b7",status:"Passed"} , {stepId:"77ec44cf-fd54-482f-874a-a6c3c556c2e1", status:"Passed"}, {stepId:"e2c47bd2-9ce2-420f-a238-cdecc609675f", status:"hold"}];
+/*let pre = [{stepId:"e6aaf568-0fbf-4efe-84f2-82b43fa3a233", status:"Passed" , pass: "Confirmed"} , {stepId:"c4b3e0b6-21d7-4e5b-aa0c-86103bc3b2b7",status:"Passed"} , {stepId:"77ec44cf-fd54-482f-874a-a6c3c556c2e1", status:"Passed"}, {stepId:"e2c47bd2-9ce2-420f-a238-cdecc609675f", status:"hold"}];
 
 
 $scope.setPrereqs = function () {
